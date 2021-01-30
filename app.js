7f71e24bd5d946321b4a8c37c288cab60417a0e2
cawleygl <grantlcawley@gmail.com>
2021-01-30 04:54:33 -0600

    Added inquirer code to collect information on team members, flexible for amount and types of members.

--- app.js
+++ app.js
@@ -10,9 +10,94 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+let engAmt = 0
+let intAmt = 0
+
+function askInitial() {
+    return inquirer
+        .prompt([
+            { type: "input", name: "mgrName", message: "What is the team manager's name?" },
+            { type: "input", name: "mgrEmail", message: "What is their email address?" },
+            { type: "input", name: "mgrOffice", message: "What is their office number?" },
+            { type: "number", name: "engAmt", message: "How many Engineers are on the team?" },
+            { type: "number", name: "intAmt", message: "How many Interns are on the team?" }
+
+        ])
+        .then(val => {
+            console.log(val.mgrName);
+            console.log(val.mgrEmail);
+            console.log(val.mgrOffice);
+            engAmt = val.engAmt
+            intAmt = val.intAmt
+            console.log(engAmt);
+            console.log(intAmt);
+
+
+            if (engAmt == 0 && intAmt == 0) {
+                console.log("You have no team members at all? Guess we're done here.");
+            } else if (isNaN(engAmt) || isNaN(intAmt)) {
+                console.log("Please enter a numerical value for the amount of engineers and interns on the team. Guess we're done here.");
+            } else if (engAmt == 0) {
+                console.log("You have no engineers, let's start with the interns.");
+                askIntern();
+            } else {
+                console.log("Let's start with the engineers.");
+                askEngineer();
+            }
+        })
+}
+
+function askEngineer() {
+    return inquirer
+        .prompt([
+            { type: "input", name: "engName", message: "Please enter the name of an Engineer on the team:" },
+            { type: "input", name: "engEmail", message: "What is their email address?" },
+            { type: "input", name: "engGithub", message: "What is their GitHub username?" },
+        ])
+        .then(val => {
+            console.log(val.engName);
+            console.log(val.engEmail);
+            console.log(val.engGithub);
+            engAmt--;
+
+            if (engAmt > 0) {
+                console.log("On to the next engineer!")
+                askEngineer();
+            } else if (intAmt == 0) {
+                console.log("No more engineers, and you have no interns, Guess we're done here!")
+            } else {
+                console.log("No more engineers, moving on the the interns!")
+                askIntern();
+            }
+        })
+}
+
+function askIntern() {
+    return inquirer
+        .prompt([
+            { type: "input", name: "intName", message: "Please enter the name of an Intern on the team:" },
+            { type: "input", name: "intEmail", message: "What is their email address?" },
+            { type: "input", name: "intSchool", message: "What school did they attend?" },
+        ])
+        .then(val => {
+            console.log(val.intName);
+            console.log(val.intEmail);
+            console.log(val.intSchool);
+            intAmt--;
+
+            if (intAmt > 0) {
+                console.log("On to the next intern!")
+                askIntern();
+            } else {
+                console.log("That should be everyone!")
+            }
+        })
+}
+
+askInitial();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
